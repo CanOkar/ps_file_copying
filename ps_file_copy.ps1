77bc7c6dcb3267a760c5b30eb636ab1d22dd12e6
CanOkar <okar.can@gmail.com>
2020-08-29 20:33:36 +0300

    ps_file_copying.ps1

--- ps_file_copy.ps1
+++ ps_file_copy.ps1
@@ -1,79 +1,46 @@
-$smartboards = Import-Csv c:\smartboards.csv 
+$smartboards = Import-Csv c:\csv_file_name.csv 
+$log_file = "c:\log_file_name.txt" 
+$destination_folder = "destination_folder_name" 
+$source_folder = "C:\source_folder_name" 
 
-$log_file = "c:\bulk_copy_log.txt" 
-
-$destination_folder = "Covid19Videos" 
-
-$source_folder = "C:\Covid19Videos" 
-
+#creates a log file if it does not exist
 $check_log_file = Test-Path -path $log_file 
-
-  
-
-  
-
 if(-Not $check_log_file) 
-
     { 
-
         New-Item $log_file -ItemType File 
-
     } 
 
 Add-Content $log_file "----------------------File copying process has just started----------------------" 
 
 foreach($smartboard in $smartboards) 
-
     { 
-
-        $smartboardx = $smartboard.name 
-
+        $smartboardx = $smartboard.name
+        
+        #return true value if the computer is reachable
         $is_computer_available = Test-Path -path "\\$smartboardx\c$\Users\Public\Desktop\" 
-
+        
+        #returns true value if the folder exist in the destination
         $path = Test-Path -path "\\$smartboardx\c$\Users\Public\Desktop\$destination_folder" 
 
-         
-
         if($is_computer_available) 
-
             { 
-
+                
                 if(-Not $path) 
-
                     { 
-
                         New-Item -Path \\$smartboardx\c$\Users\Public\Desktop\$destination_folder -ItemType directory 
-
                         Copy-Item $source_folder\*.*  \\$smartboardx\C$\Users\public\desktop\$destination_folder 
-
                         Add-Content $log_file "Files succesfully copied to $smartboardx" 
-
                         shutdown /s /m \\$smartboardx /t 0 
 
                     } 
-
                   elseif($path) 
-
                     { 
-
                         Add-Content $log_file "The folder named $destination_folder already exist at $smartboardx" 
-
                         shutdown /s /m \\$smartboardx /t 0 
-
-                    } 
-
-  
-
+                    }
             } 
-
-            else 
-
+         else 
             { 
-
                 Add-Content $log_file "It seems $smartboardx isn't available" 
-
             } 
-
-  
-
     } 
