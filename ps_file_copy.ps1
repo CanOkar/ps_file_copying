d5cd4cd1e3c581d3fd7425218323e86529bb314d
CanOkar <okar.can@gmail.com>
2020-08-29 18:11:31 +0300

    ps_file_copy.ps1

--- ps_file_copy.ps1
+++ ps_file_copy.ps1
@@ -1,40 +1,79 @@
-$smartboards = Import-Csv c:\smartboards.csv
-$log_file = "c:\bulk_copy_log.txt"
-$destination = "Covid19Videos"
-$source = "C:\Covid19Videos\*.*"
+$smartboards = Import-Csv c:\smartboards.csv 
 
-$check_log_file = Test-Path -path $log_file
+$log_file = "c:\bulk_copy_log.txt" 
 
+$destination_folder = "Covid19Videos" 
 
-if(-Not $check_log_file)
-    {
-        New-Item $log_file -ItemType File
-    }
-Add-Content $log_file "----------------------File copying process has just started----------------------"
-foreach($smartboard in $smartboards)
-    {
-        $smartboardx = $smartboard.name
-        $is_computer_available = Test-Path -path "\\$smartboardx\c$\Users\Public\Desktop\"
-        $path = Test-Path -path "\\$smartboardx\c$\Users\Public\Desktop\$destination_folder"
-        
-        if($is_computer_available)
-            {
-                if(-Not $path)
-                    {
-                        New-Item -Path \\$smartboardx\c$\Users\Public\Desktop\Covid19Videos -ItemType directory
-                        Copy-Item $source  \\$smartboardx\$destination
-                        Add-Content $log_file "Files succesfully copied to $smartboardx"
-                        shutdown /s /m \\$smartboardx /t 0
-                    }
-                  else
-                    {
-                        Add-Content $log_file "The folder named $destination already exist at $smartboardx"
-                    }
+$source_folder = "C:\Covid19Videos" 
 
-            }
-            else
-            {
-                Add-Content $log_file "It seems $smartboardx isn't available"
-            }
+$check_log_file = Test-Path -path $log_file 
 
-    }
+  
+
+  
+
+if(-Not $check_log_file) 
+
+    { 
+
+        New-Item $log_file -ItemType File 
+
+    } 
+
+Add-Content $log_file "----------------------File copying process has just started----------------------" 
+
+foreach($smartboard in $smartboards) 
+
+    { 
+
+        $smartboardx = $smartboard.name 
+
+        $is_computer_available = Test-Path -path "\\$smartboardx\c$\Users\Public\Desktop\" 
+
+        $path = Test-Path -path "\\$smartboardx\c$\Users\Public\Desktop\$destination_folder" 
+
+         
+
+        if($is_computer_available) 
+
+            { 
+
+                if(-Not $path) 
+
+                    { 
+
+                        New-Item -Path \\$smartboardx\c$\Users\Public\Desktop\$destination_folder -ItemType directory 
+
+                        Copy-Item $source_folder\*.*  \\$smartboardx\C$\Users\public\desktop\$destination_folder 
+
+                        Add-Content $log_file "Files succesfully copied to $smartboardx" 
+
+                        shutdown /s /m \\$smartboardx /t 0 
+
+                    } 
+
+                  elseif($path) 
+
+                    { 
+
+                        Add-Content $log_file "The folder named $destination_folder already exist at $smartboardx" 
+
+                        shutdown /s /m \\$smartboardx /t 0 
+
+                    } 
+
+  
+
+            } 
+
+            else 
+
+            { 
+
+                Add-Content $log_file "It seems $smartboardx isn't available" 
+
+            } 
+
+  
+
+    } 
